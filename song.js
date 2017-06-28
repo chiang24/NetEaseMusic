2a93decdca1360acf6f77f23e825ba0cf9fa57d6
chiang24 <438085855@qq.com>
2017-06-28 10:39:21 +0800

    播放歌曲页面动态获取歌曲信息

--- song.js
+++ song.js
@@ -1,10 +1,16 @@
 $(function () {
     let id = parseInt(location.search.match(/\bid=([^&]*)/)[1], 10)
-    $.get('./songs.json').then(function (Response) {
-        let songs = Response
+    $.get('./songs.json').then(function (response) {
+        let songs = response
         let song = songs.filter(s=>s.id === id)[0]
-        let {url} = song
-        let audio = document.createElement('audio')
+        let {url,name,lyric} = song
+
+        initPlayer.call(undefined,url)
+        initText(name,lyric)
+
+    })
+    function initPlayer(url){
+    let audio = document.createElement('audio')
         audio.src = url
         audio.oncanplay = function () {
             audio.play()
@@ -13,23 +19,28 @@ $(function () {
         $('.icon-pause').on('touchstart', function () {
             audio.pause()
             $('.disc-container').removeClass('playing')
-        })
+            var styles = {
+            transition:".5s ease-in-out",
+            transform:"rotateZ(-14deg)"
+       }
+            $('.pointer').css(styles)  
+    })
         $('.icon-play').on('touchstart', function () {
             audio.play()
             $('.disc-container').addClass('playing')
+            var styles = {
+           transform:"rotateZ(0deg)"
+       }
+            $('.pointer').css(styles) 
         })
+}
+    function initText(name,lyric){
+        $('.song-description>h2').text(name)
+        parseLyric(lyric)
+    }
 
-    })
-
-    $.get('./lyric.json').then(function (object) {
-        let {
-            lyric
-        } = object //let lyric = object.lyric
-        let {
-            tlyric
-        } = object
+    function parseLyric(lyric){
         let array = lyric.split('\n')
-        let tarray = tlyric.split('\n')
         let regex = /^\[(.+)\](.*)$/
         array = array.map(function (string, index) {
             let matches = string.match(regex)
@@ -40,23 +51,11 @@ $(function () {
                 }
             }
         })
-        tarray = tarray.map(function (string, index) {
-            let matches = string.match(regex)
-            if (matches) {
-                return {
-                    time: matches[1],
-                    words: matches[2]
-                }
-            }
-        })
 
         // console.log(array)
-        //  console.log(tarray)
         let $lyric = $('.lyric')
         array.map(function (object) {
-            if (!object) {
-                return
-            }
+            if (!object) {return}
             let $p = $('<p/>')
             $p.attr('data-time', object.time).text(object.words)
             $p.appendTo($lyric.children('.lines'))
@@ -69,6 +68,23 @@ $(function () {
             $p.attr('data-time', object.time).text(object.words)
             $p.appendTo($lyric.children('.lines'))
         })
+    }
+        
+
+    $.get('./songs.json').then(function(response){
+        let songs =response
+        let song = songs.filter(s=>s.id === id)[0]
+        let {backgroundImage} = song
+        let $img = $('#img')
+        $img.css("background-image",`url(${backgroundImage})`)
+    })
+        $.get('./songs.json').then(function(response){
+        let songs =response
+        let song = songs.filter(s=>s.id === id)[0]
+        let {cover} = song
+        let $cover = $('.cover')
+        console.log($cover)
+        $cover[0].src = cover
     })
 
 })
